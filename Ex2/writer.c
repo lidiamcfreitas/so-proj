46c7405d2bb517ea0b11e195249597dc5d43f5ed
Lidia <lidiamcfreitas@gmail.com>
2014-10-15 13:05:57 +0100

    added ex 2 folder

---
+++ Ex2/writer.c
@@ -0,0 +1,115 @@
+#include "writer.h"
+
+
+
+/*
+ * main program
+ */
+int main(){
+    
+    /* initializes seed, needed for rand() to work properly */
+	srand ( time(NULL) );
+
+    writer();
+    
+    return 0;
+}
+
+
+
+/* writer - opens a random file, gets a random string and writes it
+* 1024 times; repeats the process 5120 times */
+int writer(){
+    
+    /* variables initialization */
+    int i;
+    char **strings = malloc( 10 * sizeof(char *));
+    for (i = 0; i < 10; i++){
+            strings[i] = malloc(11 * sizeof(char));
+    }
+    char * path = (char *) malloc(15*sizeof(char));
+    char * buffer = (char *) malloc(11*sizeof(char));
+    
+    strings = init(strings);
+    for(i=0;i<5120;i++){
+        
+        /* opens random file, and writes random string 1024 times */
+        path = random_file(path);
+    	buffer = random_string(strings, buffer);
+    	open_write(path, buffer);
+    }
+    
+    free(path);
+    free(buffer);
+    for (i = 0; i < 10; i++){
+        free(strings[i]);
+    }
+    free(strings);
+    
+    return 0;
+}
+
+
+
+/* init - initializes vector strings */
+char** init(char** strings){
+    int i,j;
+    
+    for(i=0; i<10; i++){
+        for(j=0; j<9; j++){
+            strings[i][j] = 'a'+ i ;
+        }
+        strings[i][j] = '\n';
+        j++;
+        strings[i][j] = '\0';
+    }
+    
+    return strings;
+}
+
+
+
+/* random_file - changes path to a random file name of 5 possible */
+char* random_file(char* path){
+    int i;
+    
+    i = rand() % 5;
+    sprintf(path, "./SO2014-%d.txt", i);
+    
+    return path;
+}
+
+
+
+/* random_string - modifies buffer to a random string from vector strings */
+char* random_string(char** strings, char* buffer){
+    int i;
+    i = rand() % 10;
+    
+    strcpy(buffer, strings[i]);
+    
+    return buffer;
+}
+
+
+
+/* open_write - opens a file and writes a string 1024 times */
+void open_write(char* path, char* buffer){
+    int i, file_descriptor, size_buffer;
+    size_buffer = sizeof(char)*11;
+
+    /* mode - user has read permission, file owner has read, write and exec
+    *         others have read permission */
+    mode_t mode = S_IRWXU | S_IRUSR | S_IROTH;
+    
+    file_descriptor = open(path, O_CREAT|O_WRONLY|O_TRUNC , mode);
+    
+    for(i=0;i<1024;i++){
+    write(file_descriptor, buffer, size_buffer);
+    }
+
+    close(file_descriptor);
+}
+
+
+
